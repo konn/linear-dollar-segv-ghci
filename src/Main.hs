71da9ec6959579e6f265db4ea428c570007db8ab
Hiromi ISHII <konn.jinro@gmail.com>
2023-09-17 14:19:58 +0900

    Minimise example

--- src/Main.hs
+++ src/Main.hs
@@ -6,18 +6,12 @@ module Main (main) where
 data Ur a where
   Ur :: a -> Ur a
 
-unur :: Ur a %1 -> a
+unur :: Ur a -> a
 unur (Ur a) = a
 
-lseq :: () %1 -> a %1 -> a
-lseq () a = a
-
-segvGHCi :: () %1 -> Either (Ur (Maybe ())) ()
-segvGHCi u = u `lseq` Left (Ur $ Just ())
-
-eitherL :: (a %1 -> c) -> (b %1 -> c) -> Either a b %1 -> c
-eitherL f _ (Left x) = f x
-eitherL _ g (Right y) = g y
+segvGHCi :: Ur ()
+{- HLINT ignore segvGHCi "Redundant $" -}
+segvGHCi = Ur $ ()
 
 main :: IO ()
-main = print (unur (eitherL (\x -> x) (`lseq` Ur Nothing) (segvGHCi ())))
+main = print (unur segvGHCi)
